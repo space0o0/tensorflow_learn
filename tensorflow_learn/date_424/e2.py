fe0403b8d5db517b53729a8e240b6d974f159c09
space0o0-Macbook pro <799429639@qq.com>
2017-04-25 23:16:22 +0800

    提交

--- tensorflow_learn/date_424/e2.py
+++ tensorflow_learn/date_424/e2.py
@@ -2,11 +2,11 @@ import tensorflow as tf
 
 g1 = tf.Graph()
 with g1.as_default():
-    v = tf.get_variable("v", initializer=1., dtype=tf.float32)
+    tf.get_variable("v", initializer=1.1, dtype=tf.float32)
 
 g2 = tf.Graph()
 with g2.as_default():
-    v = tf.get_variable("v", initializer=2., dtype=tf.float32)
+    tf.get_variable("v", initializer=2., dtype=tf.float32)
 
 with tf.Session(graph=g1) as sess:
     tf.global_variables_initializer().run()
@@ -17,3 +17,5 @@ with tf.Session(graph=g2) as sess:
     tf.global_variables_initializer().run()
     with tf.variable_scope("", reuse=True):
         print (sess.run(tf.get_variable("v")))
+
+
---
+++ tensorflow_learn/udacity_assignment1/assignment.py
@@ -0,0 +1,17 @@
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import tarfile
+from IPython.display import display, Image
+from py2app.recipes import matplotlib
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle
+
+# Config the matplotlib backend as plotting inline in IPython
+from tensorflow.contrib.distributions.python.ops.bijectors import inline
